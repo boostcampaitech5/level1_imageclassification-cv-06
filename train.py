38c7e5a40b229b40ce18780978d96cf5d4aeffa9
jemin <jemin7709@gmail.com>
2023-04-14 02:57:13 +0000

    fix: argparse resize에서 type을 list에서 int로 수정
    - argparse resize type을 list에서 int로 수정
    - black, ruff 적용

--- train.py
+++ train.py
@@ -31,7 +31,7 @@ def seed_everything(seed):
 
 def get_lr(optimizer):
     for param_group in optimizer.param_groups:
-        return param_group['lr']
+        return param_group["lr"]
 
 
 def grid_image(np_images, gts, preds, n=16, shuffle=False):
@@ -41,7 +41,7 @@ def grid_image(np_images, gts, preds, n=16, shuffle=False):
     choices = random.choices(range(batch_size), k=n) if shuffle else list(range(n))
     figure = plt.figure(figsize=(12, 18 + 2))  # cautions: hardcoded, 이미지 크기에 따라 figsize 를 조정해야 할 수 있습니다. T.T
     plt.subplots_adjust(top=0.8)  # cautions: hardcoded, 이미지 크기에 따라 top 를 조정해야 할 수 있습니다. T.T
-    n_grid = int(np.ceil(n ** 0.5))
+    n_grid = int(np.ceil(n**0.5))
     tasks = ["mask", "gender", "age"]
     for idx, choice in enumerate(choices):
         gt = gts[choice].item()
@@ -49,11 +49,9 @@ def grid_image(np_images, gts, preds, n=16, shuffle=False):
         image = np_images[choice]
         gt_decoded_labels = MaskBaseDataset.decode_multi_class(gt)
         pred_decoded_labels = MaskBaseDataset.decode_multi_class(pred)
-        title = "\n".join([
-            f"{task} - gt: {gt_label}, pred: {pred_label}"
-            for gt_label, pred_label, task
-            in zip(gt_decoded_labels, pred_decoded_labels, tasks)
-        ])
+        title = "\n".join(
+            [f"{task} - gt: {gt_label}, pred: {pred_label}" for gt_label, pred_label, task in zip(gt_decoded_labels, pred_decoded_labels, tasks)]
+        )
 
         plt.subplot(n_grid, n_grid, idx + 1, title=title)
         plt.xticks([])
@@ -65,7 +63,7 @@ def grid_image(np_images, gts, preds, n=16, shuffle=False):
 
 
 def increment_path(path, exist_ok=False):
-    """ Automatically increment path, i.e. runs/exp --> runs/exp0, runs/exp1 etc.
+    """Automatically increment path, i.e. runs/exp --> runs/exp0, runs/exp1 etc.
 
     Args:
         path (str or pathlib.Path): f"{model_dir}/{args.name}".
@@ -76,7 +74,7 @@ def increment_path(path, exist_ok=False):
         return str(path)
     else:
         dirs = glob.glob(f"{path}*")
-        matches = [re.search(rf"%s(\d+)" % path.stem, d) for d in dirs]
+        matches = [re.search(r"%s(\d+)" % path.stem, d) for d in dirs]
         i = [int(m.groups()[0]) for m in matches if m]
         n = max(i) + 1 if i else 2
         return f"{path}{n}"
@@ -130,24 +128,18 @@ def train(data_dir, model_dir, args):
 
     # -- model
     model_module = getattr(import_module("model.base_model"), args.model)  # default: BaseModel
-    model = model_module(
-        num_classes=num_classes
-    ).to(device)
+    model = model_module(num_classes=num_classes).to(device)
     model = torch.nn.DataParallel(model)
 
     # -- loss & metric
     criterion = create_criterion(args.criterion)  # default: cross_entropy
     opt_module = getattr(import_module("torch.optim"), args.optimizer)  # default: SGD
-    optimizer = opt_module(
-        filter(lambda p: p.requires_grad, model.parameters()),
-        lr=args.lr,
-        weight_decay=5e-4
-    )
+    optimizer = opt_module(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr, weight_decay=5e-4)
     scheduler = StepLR(optimizer, args.lr_decay_step, gamma=0.5)
 
     # -- logging
     logger = SummaryWriter(log_dir=save_dir)
-    with open(os.path.join(save_dir, 'config.json'), 'w', encoding='utf-8') as f:
+    with open(os.path.join(save_dir, "config.json"), "w", encoding="utf-8") as f:
         json.dump(vars(args), f, ensure_ascii=False, indent=4)
 
     best_val_acc = 0
@@ -212,9 +204,7 @@ def train(data_dir, model_dir, args):
                 if figure is None:
                     inputs_np = torch.clone(inputs).detach().cpu().permute(0, 2, 3, 1).numpy()
                     inputs_np = dataset_module.denormalize_image(inputs_np, dataset.mean, dataset.std)
-                    figure = grid_image(
-                        inputs_np, labels, preds, n=16, shuffle=args.dataset != "MaskSplitByProfileDataset"
-                    )
+                    figure = grid_image(inputs_np, labels, preds, n=16, shuffle=args.dataset != "MaskSplitByProfileDataset")
 
             val_loss = np.sum(val_loss_items) / len(val_loader)
             val_acc = np.sum(val_acc_items) / len(val_set)
@@ -224,39 +214,36 @@ def train(data_dir, model_dir, args):
                 torch.save(model.module.state_dict(), f"{save_dir}/best.pth")
                 best_val_acc = val_acc
             torch.save(model.module.state_dict(), f"{save_dir}/last.pth")
-            print(
-                f"[Val] acc : {val_acc:4.2%}, loss: {val_loss:4.2} || "
-                f"best acc : {best_val_acc:4.2%}, best loss: {best_val_loss:4.2}"
-            )
+            print(f"[Val] acc : {val_acc:4.2%}, loss: {val_loss:4.2} || " f"best acc : {best_val_acc:4.2%}, best loss: {best_val_loss:4.2}")
             logger.add_scalar("Val/loss", val_loss, epoch)
             logger.add_scalar("Val/accuracy", val_acc, epoch)
             logger.add_figure("results", figure, epoch)
             print()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # Data and model checkpoints directories
-    parser.add_argument('--seed', type=int, default=42, help='random seed (default: 42)')
-    parser.add_argument('--epochs', type=int, default=1, help='number of epochs to train (default: 1)')
-    parser.add_argument('--dataset', type=str, default='MaskBaseDataset', help='dataset augmentation type (default: MaskBaseDataset)')
-    parser.add_argument('--augmentation', type=str, default='BaseAugmentation', help='data augmentation type (default: BaseAugmentation)')
-    parser.add_argument("--resize", nargs="+", type=list, default=[128, 96], help='resize size for image when training')
-    parser.add_argument('--batch_size', type=int, default=64, help='input batch size for training (default: 64)')
-    parser.add_argument('--valid_batch_size', type=int, default=1000, help='input batch size for validing (default: 1000)')
-    parser.add_argument('--model', type=str, default='BaseModel', help='model type (default: BaseModel)')
-    parser.add_argument('--optimizer', type=str, default='SGD', help='optimizer type (default: SGD)')
-    parser.add_argument('--lr', type=float, default=1e-3, help='learning rate (default: 1e-3)')
-    parser.add_argument('--val_ratio', type=float, default=0.2, help='ratio for validaton (default: 0.2)')
-    parser.add_argument('--criterion', type=str, default='cross_entropy', help='criterion type (default: cross_entropy)')
-    parser.add_argument('--lr_decay_step', type=int, default=20, help='learning rate scheduler deacy step (default: 20)')
-    parser.add_argument('--log_interval', type=int, default=20, help='how many batches to wait before logging training status')
-    parser.add_argument('--name', default='exp', help='model save at {SM_MODEL_DIR}/{name}')
+    parser.add_argument("--seed", type=int, default=42, help="random seed (default: 42)")
+    parser.add_argument("--epochs", type=int, default=1, help="number of epochs to train (default: 1)")
+    parser.add_argument("--dataset", type=str, default="MaskBaseDataset", help="dataset augmentation type (default: MaskBaseDataset)")
+    parser.add_argument("--augmentation", type=str, default="BaseAugmentation", help="data augmentation type (default: BaseAugmentation)")
+    parser.add_argument("--resize", nargs="+", type=int, default=[128, 96], help="resize size for image when training")
+    parser.add_argument("--batch_size", type=int, default=64, help="input batch size for training (default: 64)")
+    parser.add_argument("--valid_batch_size", type=int, default=1000, help="input batch size for validing (default: 1000)")
+    parser.add_argument("--model", type=str, default="BaseModel", help="model type (default: BaseModel)")
+    parser.add_argument("--optimizer", type=str, default="SGD", help="optimizer type (default: SGD)")
+    parser.add_argument("--lr", type=float, default=1e-3, help="learning rate (default: 1e-3)")
+    parser.add_argument("--val_ratio", type=float, default=0.2, help="ratio for validaton (default: 0.2)")
+    parser.add_argument("--criterion", type=str, default="cross_entropy", help="criterion type (default: cross_entropy)")
+    parser.add_argument("--lr_decay_step", type=int, default=20, help="learning rate scheduler deacy step (default: 20)")
+    parser.add_argument("--log_interval", type=int, default=20, help="how many batches to wait before logging training status")
+    parser.add_argument("--name", default="exp", help="model save at {SM_MODEL_DIR}/{name}")
 
     # Container environment
-    parser.add_argument('--data_dir', type=str, default=os.environ.get('SM_CHANNEL_TRAIN', '/opt/ml/input/data/train/images'))
-    parser.add_argument('--model_dir', type=str, default=os.environ.get('SM_MODEL_DIR', './experiment'))
+    parser.add_argument("--data_dir", type=str, default=os.environ.get("SM_CHANNEL_TRAIN", "/opt/ml/input/data/train/images"))
+    parser.add_argument("--model_dir", type=str, default=os.environ.get("SM_MODEL_DIR", "./experiment"))
 
     args = parser.parse_args()
     print(args)
