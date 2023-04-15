432d7c6f162acf4908683bc3307173fc56a01894
moons98 <munsi2003@gmail.com>
2023-04-15 09:01:58 +0000

    fix: 잘못된 변수 이름 수정

--- code_book/background_img_check.py
+++ code_book/background_img_check.py
@@ -5,16 +5,17 @@ folder_lst = os.listdir(BASE_ROOT)
 
 img = []
 img_duplication = []  # 중복되는 이미지가 있는지 확인하기 위해 list 2개를 나눠서 선언
-for i in folder_lst:
-    img_foler_path = os.path.join(BASE_ROOT, i)
+for lst in folder_lst:
+    img_folder_path = os.path.join(BASE_ROOT, lst)
 
-    img_lst = os.listdir(img_foler_path)
-
+    img_lst = os.listdir(img_folder_path)  # img가 담겨 있는 각 폴더까지의 경로
     for j in img_lst:
-        img_path_name = os.path.join(img_foler_path, j)  # 경로를 포함한 이미지 이름
+        img_path_name = os.path.join(img_folder_path, j)  # 경로를 포함한 이미지 이름
         if img_path_name not in img:
             img.append(img_path_name)
         else:
             img_duplication.append(img_path_name)
 
-print(len(img), len(img_duplication))  # 중복되지 않는 경우, lst1만 18900이라는 값을 출력해야 정상
+# 중복되지 않는 경우, lst1만 18900이라는 값을 출력해야 정상
+if len(img) == 18900 and len(img_duplication) == 0:
+    print(f"Img is correctly set! \n{len(img)} background subtracted images exist.")
