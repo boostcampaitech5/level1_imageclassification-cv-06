cd1af3910652efd5aa2ee7c97c9f601ce1f8e2fe
moons98 <munsi2003@gmail.com>
2023-04-15 15:54:57 +0000

    feat: my_dataset.py 상속 인자 수정

--- model/my_model.py
+++ model/my_model.py
@@ -22,7 +22,7 @@ class MyModel(BaseModel):
 
 class EfficientNetB3(BaseModel):
     def __init__(self, num_classes=18):
-        super(MyModel, self).__init__()
+        super(BaseModel, self).__init__()
         self.my_efficientnetb3 = models.efficientnet_b4(pretrained=True)
         self.my_efficientnetb3.classifier = nn.Linear(1536, num_classes)
         print("권장 크기는 300 * 300 size 입니다.")
@@ -35,7 +35,7 @@ class EfficientNetB3(BaseModel):
 
 class EfficientNetB4(BaseModel):
     def __init__(self, num_classes=18):
-        super(MyModel, self).__init__()
+        super(BaseModel, self).__init__()
         self.my_efficientnetb4 = models.efficientnet_b4(pretrained=True)
         self.my_efficientnetb4.classifier = nn.Linear(1792, num_classes)
         print("권장 크기는 380 * 380 size 입니다.")
